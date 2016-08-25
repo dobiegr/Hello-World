5aa33b610147373a50714afeb2093dead33138a8
dobiegr <github@dobie-family.co.uk>
2016-08-26 00:02:42 +0100

    Delete vlcrec3.bat

--- vlcrec3.bat
+++
@@ -1,8 +0,0 @@
-c:
-pause
-cd "C:\Program Files\VideoLAN\VLC\"
-pause
-
-vlc.exe -vvv dshow:// :dshow-vdev="Microsoft LifeCam Cinema" :dshow-size="320x240"--sout="#transcode vcodec=h264,vb=800,acodec=aac,ab=128}:standard{mux=ts,dst=\"C:test.mpg\",access=file}"
-
-pause
